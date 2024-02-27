4a2d77ed090fd203b83966f4e12dab05ec464755
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-02-27 07:23:36 -0300

    a

--- aula1.js
+++ aula1.js
@@ -1,10 +1,10 @@
 console.log('Oi, tudo bem!'); 
-// Coloca uma informção no console
+// Coloca uma informção no console;
 alert("Escrevendo no navegador");
-// Aparece um alerta no site
+// Aparece um alerta no site;
 let dia = prompt("Que dia é hoje?");
-// Pega o input que o usuário escrever
+// Pega o input que o usuário escrever;
 console.log("Hoje é " + dia);
 alert("Hoje é " + dia);
 document.querySelector('#dia').innerHTML = 'Hoje é ' + dia;
-// Três formas de aparecer o input que o site pegou
+// Três formas de aparecer o input que o site pegou;
