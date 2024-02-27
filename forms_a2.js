794978e08f1389565db3598755da57ec2dc1a6a3
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-02-27 08:19:02 -0300

    a

--- forms_a2.js
+++ forms_a2.js
@@ -1,13 +1,20 @@
 function submeterFormulario() {
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    // pega o elemento da caixa pelo id
-    // tem que especificar esse "value" pra ele pegar a informação de dentro da caixa
-
-    document.getElementById("resultado").innerHTML = 
-        "<h3>Informações Recebidas</h3>" +
-        // o número no "h3" é o tamanho do texto
-        "<p>Nome: " + nome + "</p>" +
-        "<p>E-mail: " + email + "</p>";
-    // escreve isso dentro daquela div (a caixinha virtual)
+    let confirma = confirm("Você confirma a ação?");
+    if(confirma == true) {
+        let nome = document.getElementById("nome").value;
+        let email = document.getElementById("email").value;
+        // pega o elemento da caixa pelo id
+        // tem que especificar esse "value" pra ele pegar a informação de dentro da caixa
+        document.getElementById("resultado").innerHTML = 
+            "<h3>Informações Recebidas</h3>" +
+            // o número no "h3" é o tamanho do texto
+            "<p>Nome: " + nome + "</p>" +
+            "<p>E-mail: " + email + "</p>";
+        // escreve isso dentro daquela div (a caixinha virtual)
+    }
+else {
+    document.getElementById("resultado").innerHTML =
+    "<h2>Ação cancelada</h2>";
+    }
+// se der "false", ele cancela a ação
 }
