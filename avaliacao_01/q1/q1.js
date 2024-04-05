90a8d0e55ca72f094124cc57eb9aedde88aa6a15
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-04-05 08:12:11 -0300

    Questões do Teste 01.

--- avaliacao_01/q1/q1.js
+++ avaliacao_01/q1/q1.js
@@ -4,7 +4,11 @@ function repetirF() {
     let n = document.getElementById('numero').value;
 
     document.getElementById('resultado').innerHTML = "";
+    // Limpa o conteúdo da div resultado
+    
+    // Loop que repete a frase o número de vezes especificado
     for (let i = 0; i < n; i++) {
+        // Adiciona a frase seguida de uma quebra de linha à div resultado
         document.getElementById('resultado').innerHTML += 
             `${frase} <br>`; 
     }
--- avaliacao_01/q2/q2.js
+++ avaliacao_01/q2/q2.js
@@ -1,13 +1,18 @@
 
 function calcularD() {
+    // Obtém as coordenadas x e y do primeiro ponto inseridas pelo usuário
     const x1 = document.getElementById('x1').value;
     const y1 = document.getElementById('y1').value;
+    // Obtém as coordenadas x e y do segundo ponto inseridas pelo usuário
     const x2 = document.getElementById('x2').value;
     const y2 = document.getElementById('y2').value;
 
+    // Calcula a distância euclidiana entre os dois pontos
     const dist = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
 
+    // Exibe o resultado na div com id "resultado"
     document.getElementById('resultado').innerHTML = 
     `A distância entre P1(${x1}, ${y1}) e P2(${x2}, ${y2})
      é igual a <b>${dist.toFixed(2)}</b>`;
+    // Coloca o resultado em negrito com o "<b></b>""
 }
