d388b25aecb35b7f693ab6179c9ff5aedda75cfd
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-07-16 08:37:43 -0300

    Aula da questão 21 da lista.

---
+++ lista_1/q21/q21.js
@@ -0,0 +1,19 @@
+/* Desenvolva um formulário para cadastro de produtos que inclua nome do produto, 
+preço, categoria (dropdown com opções pré-definidas) e descrição. 
+Crie uma função JavaScript que adicione cada produto cadastrado a uma tabela HTML
+abaixo do formulário, atualizando-a dinamicamente a cada novo cadastro. */
+
+function cadastrar() {
+    const nome = document.getElementById("nome").value
+    const preco = document.getElementById("preco").value
+    const categoria = document.getElementById("categoria").value
+    const descricao = document.getElementById("descricao").value
+
+    let linha = document.getElementById("tabela").insertRow();
+    linha.insertCell(0).textContent = nome;
+    linha.insertCell(1).textContent = preco;
+    linha.insertCell(2).textContent = categoria;
+    linha.insertCell(3).textContent = descricao;
+
+    document.getElementById('formProduto').reset();
+}
