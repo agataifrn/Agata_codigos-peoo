7345ca2dd77627d8a7fe56e5d4608e2c23f3a707
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-07-22 08:25:59 -0300

    Aula sobre matrizes.

--- TEORIA/matrizes/matrizes.js
+++ TEORIA/matrizes/matrizes.js
@@ -10,10 +10,18 @@ const B = [
     [14, 16, 18]
 ];
 
+const C = somaM(A, B);
+
 function somaM() {
+    /* Cria uma matriz pro resultado com um vetor vazio, 
+    pra depois criar outros e preencher-los */
     let resultado = [];
+
+    // Percorre a linha (horizontal)
     for (let i = 0; i < A.length; i++) {
             resultado[i] = [];
+
+            // Percorre a coluna (vertical)
             for (let j = 0; j < A[i].length; j++) {
                     resultado[i][j] = A[i][j] + B[i][j];
             }
@@ -21,7 +29,6 @@ function somaM() {
     return resultado;
 }
 
-const C = somaM(A, B);
 console.table(A);
 console.table(B);
 console.table(C);
