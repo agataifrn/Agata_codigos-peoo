9989fcd4d4654367e8862450d396a8809ecb80db
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-11-05 11:53:32 -0300

    Conceitos de POO.

--- TypeScript/circulo.js
+++ TypeScript/circulo.js
@@ -1,9 +1,28 @@
 var circulo = /** @class */ (function () {
+    // Classe (com atributos privados, para nao poder acessar nem alterar)
     function circulo(raio, x, y) {
         this.raio = raio;
         this.x = x;
         this.y = y;
-    }
+    } // Molde da classe pra criar objeto
+    circulo.prototype.getRaio = function () {
+        return this.raio;
+    }; // Metódo getter
+    circulo.prototype.setRaio = function (r) {
+        if (r <= 0)
+            console.log('Não dá pra ter raio negativo!!');
+        else if (r > 15)
+            console.log('Raio não alterado, limite ultrapassado.');
+        else
+            this.raio = r;
+    }; // Método setter
+    circulo.prototype.mover = function (x, y) {
+        this.x += x; // this.x = this.x + x;
+        this.y += y; // this.y = this.y + y;
+    };
+    circulo.prototype.aumentar = function (r) {
+        this.raio += r;
+    };
     circulo.prototype.exibir = function () {
         return "Raio: ".concat(this.raio, ", X: ").concat(this.x, ", Y: ").concat(this.y);
     };
@@ -11,5 +30,18 @@ var circulo = /** @class */ (function () {
 }());
 var circulo1 = new circulo(2, 0, 0);
 var circulo2 = new circulo(3, 5, 2);
+// Objetos criados
 console.log(circulo1.exibir());
+console.log(circulo2.exibir());
+circulo1.aumentar(5);
+console.log(circulo1.exibir());
+circulo2.mover(2, 4);
+console.log(circulo2.exibir());
+/* circulo2.raio = 10; -> tá errado isso, ta alterando diretamente o atributo
+console.log(circulo2.raio); -> tbm tá errado, n pode ler diretamente
+Pra fazer isso de forma correta, tem q usar o Encapsulamento, usando Setter e Getter,
+metódos usados para definir (setter) e ler (getter) os atributos.
+ */
+circulo2.setRaio(-4);
+console.log(circulo2.getRaio());
 console.log(circulo2.exibir());
--- TypeScript/circulo.ts
+++ TypeScript/circulo.ts
@@ -1,20 +1,61 @@
 class circulo {
-    raio: number;
-    x: number;
-    y: number;
+    private raio: number;
+    private x: number;
+    private y: number;
+    // Classe (com atributos privados, para nao poder acessar nem alterar)
 
     constructor(raio: number, x: number, y: number) {
         this.raio = raio;
         this.x = x;
         this.y = y;
+    } // Molde da classe pra criar objeto
 
-    }
-    exibir(): string {
-        return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;
-    }
+    getRaio() : number {
+        return this.raio;
+    } // Metódo getter
+    
+    setRaio(r: number) : void {
+        if (r <= 0)
+            console.log('Não dá pra ter raio negativo!!')
+        else if (r > 15)
+            console.log('Raio não alterado, limite ultrapassado.')
+        else
+            this.raio = r;
+    } // Método setter
+
+
+        mover(x: number, y: number) : void { // Void =  não tem retorno
+            this.x += x; // this.x = this.x + x;
+            this.y += y; // this.y = this.y + y;
+        }
+        aumentar(r: number) : void {
+            this.raio += r;
+        }
+        exibir(): string {
+            return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;
+        }
+        // Métodos (funções)
 }
 
 let circulo1 = new circulo(2, 0, 0);
 let circulo2 = new circulo(3, 5, 2);
+// Objetos criados
+
 console.log(circulo1.exibir());
+console.log(circulo2.exibir());
+
+circulo1.aumentar(5);
+console.log(circulo1.exibir());
+
+circulo2.mover(2, 4);
+console.log(circulo2.exibir());
+
+/* circulo2.raio = 10; -> tá errado isso, ta alterando diretamente o atributo
+console.log(circulo2.raio); -> tbm tá errado, n pode ler diretamente
+Pra fazer isso de forma correta, tem q usar o Encapsulamento, usando Setter e Getter,
+metódos usados para definir (setter) e ler (getter) os atributos.
+ */
+
+circulo2.setRaio(-4);
+console.log(circulo2.getRaio());
 console.log(circulo2.exibir());
