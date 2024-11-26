f0e535f8b7d94fd99d3acfaeacfd6c18daa7eb17
Ágata <freire.agata@escolar.ifrn.edu.br>
2024-11-26 12:16:27 -0300

    Q2 da avaliacao

--- TypeScript/PRÁTICA/AVALIAÇÕES/AV1/Q2_filmes/Filmes.ts
+++ TypeScript/PRÁTICA/AVALIAÇÕES/AV1/Q2_filmes/Filmes.ts
@@ -1,19 +1,37 @@
 export class Filme {
-    private titulo : string;
-    private diretor : string;
-    private anoLancamento : number;
+    private _titulo : string;
+    private _diretor : string;
+    private _anoLancamento : number;
     private avaliacoes : number;
 
-    constructor() {
-        
+/*     private avaliacao : number
+    private avaliacoes : avaliacao[]; // ????? */
+
+    constructor(titulo : string, diretor : string, anoLancamento : number, avaliacoes : number) {
+        this._titulo = titulo;
+        this._diretor = diretor;
+        this._anoLancamento = anoLancamento;
+        this.avaliacoes = avaliacoes;
     }
+
+    get titulo() : string { return this._titulo; }
+    set titulo(titulo: string) { this._titulo = titulo; }
+
+    get diretor() : string { return this._diretor; }
+    set diretor(diretor: string) { this._diretor = diretor; }
+
+    get anoLancamento() : number { return this._anoLancamento; }
+    set anoLancamento(anoLancamento: number) { this._anoLancamento = anoLancamento; }
+
+    
+
 }
 
 /* 
 Crie uma classe Filme com as propriedades: titulo (string), diretor (string),
-anoLancamento (number) e avaliacoes (array de numbers).
+anoLancamento (number) e avaliacoes (array de numbers). X
 
-Crie getters e setters para os atributos titulo, diretor e anoLancamento. 
+Crie getters e setters para os atributos titulo, diretor e anoLancamento. X
 
 Implemente métodos para adicionar uma avaliação ao array (adicionarAvaliacao(nota: number)) 
 e para calcular a média das avaliações (calcularMediaAvaliacoes()). 
